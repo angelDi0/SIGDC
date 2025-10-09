ff47ef1a668f627ce27c49c280ae2348cf91ee9e
angelDi0 <angel.diosdado1239@gmail.com>
2025-10-08 22:11:53 -0600

    Integracion de una template de prueba

--- SIGDC/usuarios/urls.py
+++ SIGDC/usuarios/urls.py
@@ -3,4 +3,5 @@ from usuarios import views
 
 urlpatterns = [
     path('', views.PerfilList.as_view(), name='perfil-list'),
+    path('index/', views.index, name='index'),
 ]
--- SIGDC/usuarios/views.py
+++ SIGDC/usuarios/views.py
@@ -19,3 +19,8 @@ class PerfilList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def index(request):
+    return render(request, 'usuarios/index.html')
+
